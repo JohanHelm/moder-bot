9d355c08ac49cdae3fe3d23acd3e0c47b014f165
alexander <sadikov.af@ssmu.ru>
2024-12-27 10:29:31 +0700

    added mute for time, added admins  notification about report

--- handlers/commands_handler.py
+++ handlers/commands_handler.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message, ChatPermissions
+from aiogram.exceptions import TelegramForbiddenError
 
 from utils.chats_admins import AdminsManager
 from lexicon.response_gens import you_have_to_answer, u_r_not_admin
@@ -25,13 +26,16 @@ async def ban_user(msg: Message, admins_manager: AdminsManager):
 
 @commands_router.message(Command(commands='mute'))
 async def mute_user(msg: Message, admins_manager: AdminsManager):
+    mute_time = msg.text.replace("/mute", "").strip()
+    if not mute_time and not mute_time.isdigit():
+        mute_time = 5
+
     if msg.from_user.id in admins_manager.chat_admins[msg.chat.id]:
         if msg.reply_to_message:
-            # TODO добавить возможность задавать время мута
             await msg.bot.restrict_chat_member(msg.chat.id,
                                                msg.reply_to_message.from_user.id,
-                                               ChatPermissions(can_send_messages = False),
-                                               until_date = timedelta(minutes = 5))
+                                               ChatPermissions(can_send_messages=False),
+                                               until_date=timedelta(minutes=mute_time))
             await msg.bot.delete_message(msg.chat.id, msg.message_id)
         else:
             await you_have_to_answer(msg)
@@ -42,11 +46,11 @@ async def mute_user(msg: Message, admins_manager: AdminsManager):
 @commands_router.message(Command(commands='report'))
 async def report_user(msg: Message, admins_manager: AdminsManager):
     if msg.reply_to_message:
-        # TODO рассылкой уведомить админов о репорте
-        print(msg.text)
-        # await msg.bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id,
-        #                                    ChatPermissions(can_send_messages=False), until_date=timedelta(minutes=5))
-        # await msg.bot.delete_message(msg.chat.id, msg.message_id)
+        for admin_id in admins_manager.chat_admins[msg.chat.id]:
+            try:
+                await msg.bot.forward_message(admin_id, msg.chat.id, msg.reply_to_message.message_id)
+            except TelegramForbiddenError:
+                print(f"bot was blocked by the admin with id {admin_id}")
     else:
         await you_have_to_answer(msg)
 
